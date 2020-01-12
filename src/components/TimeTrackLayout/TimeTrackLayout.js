62a08e3ebc568d148c1835d4db0f552ea0d74c50
Steve Willson <steve.willson@gmail.com>
2020-01-12 02:08:08 -0800

    update layout size

--- src/components/TimeTrackLayout/TimeTrackLayout.js
+++ src/components/TimeTrackLayout/TimeTrackLayout.js
@@ -50,9 +50,9 @@ const TimeTrackLayout = () => {
         onChange={(date) => dispatch({ type: 'DATECHANGE', payload: { date: date }})} //only when value has changed
       />
       <button style={btnStyle} onClick={() => dispatch({ type: 'EXPORTDATA' })}>Export</button>  
-      <RGL className="layout" cols={2} rowHeight={300} width={1200} >
-        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}><TimePlan /></div>
-        <div key="b" data-grid={{x: 1, y: 0, w: 1, h: 2, minW: 2, maxW: 4}}><TrackedTime /></div>
+      <RGL className="layout" cols={2} rowHeight={100} width={1200} >
+        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2}}><TimePlan /></div>
+        <div key="b" data-grid={{x: 1, y: 0, w: 1, h: 2}}><TrackedTime /></div>
         <div key="c" data-grid={{x: 0, y: 2, w: 1, h: 2}}><TodoList /></div>
         <div key="d" data-grid={{x: 1, y: 2, w: 1, h: 2}}><NotesList /></div>
       </RGL>
