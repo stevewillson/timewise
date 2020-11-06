e27d9b81afb1ab7fdcc574a5b61ecd733fae7cb9
Steve Willson <steve.willson@gmail.com>
2020-11-06 06:13:13 -0500

    remove react router requirement for explicit endpoint

--- src/assets/App.js
+++ src/assets/App.js
@@ -1,23 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
 import Header from '../components/layout/Header';
 import TimeTrackLayout from '../components/TimeTrackLayout/TimeTrackLayout';
 import './App.css';
 
-class App extends Component {
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/" render={() => (
-            <TimeTrackLayout />
-          )} />
-        </div>
-      </Router>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <TimeTrackLayout />
+    </div>
+  );
 }
 
 export default App;
